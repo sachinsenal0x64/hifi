ac1e62a069ecbe1753389f429f8800c3115d953d
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-10-30 08:23:52 +0800

    Remove cache control and content type headers from RewriteRequest function

--- middleware/rewrite.go
+++ middleware/rewrite.go
@@ -270,9 +270,6 @@ func RewriteRequest(w http.ResponseWriter, r *http.Request) {
 			})
 		}
 
-		w.Header().Set("Cache-Control", "public, max-age=86400")
-		w.Header().Set("Content-Type", "application/json")
-
 		json.NewEncoder(w).Encode(sub)
 
 	// -------------------- Stream --------------------
