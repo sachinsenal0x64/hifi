c776e672f08a5be66547dc1ba255cb84599d20bc
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-12-03 23:38:56 +0800

    Adjust padding in grid layout for improved spacing in +page.svelte

--- web/ui/src/routes/deactivate/+page.svelte
+++ web/ui/src/routes/deactivate/+page.svelte
@@ -42,7 +42,7 @@
 			</TabsList>
 
 			<div
-				class="grid w-full grid-cols-1 gap-4 px-4 pb-20 md:grid-cols-2 md:overflow-y-auto md:px-20"
+				class="grid w-full grid-cols-1 gap-4 px-4 pb-20 md:grid-cols-2 md:overflow-y-auto md:px-10"
 			>
 				<div
 					class="border-border relative col-span-1 min-w-[300px] rounded-lg border bg-zinc-900 p-8 md:col-span-2 md:min-h-[480px] md:overflow-y-hidden"
