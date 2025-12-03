7b70d1a30bedc1a9917c9ae195f7973387adc5e1
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-12-04 02:04:39 +0800

    Add Connect and Profile tabs with associated content in +page.svelte

--- web/ui/src/routes/deactivate/+page.svelte
+++ web/ui/src/routes/deactivate/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Deactivate from '$lib/components/Account.svelte';
 	import Header from '$lib/components/Header.svelte';
+	import Profile from '$lib/components/Profile.svelte';
 	import { Tabs, TabsContent, TabsList, TabsTrigger } from '$lib/components/ui/tabs';
 	import { Unplug, ShieldMinus } from 'lucide-svelte';
+	import Textarea from '$lib/components/Connect.svelte';
 	import { goto } from '$app/navigation';
 
 	let currentTab = 'tab-6';
@@ -25,7 +27,7 @@
 				<TabsTrigger
 					value="tab-5"
 					onclick={() => goto('/connect')}
-					class="hidden w-full cursor-pointer items-center justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white md:flex"
+					class="hidden w-full cursor-pointer items-center  justify-start gap-3 rounded-md px-6 py-4 text-3xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white md:flex"
 				>
 					<Unplug class="h-8 w-8" /> Connect
 				</TabsTrigger>
@@ -45,7 +47,7 @@
 				<div
 					class="border-border relative col-span-1 min-w-[300px] rounded-lg border bg-zinc-900 p-8 md:col-span-2 md:min-h-[480px] md:overflow-y-hidden"
 				>
-					<TabsContent value="tab-6" class="min-h-[480px] md:h-[600px] xl:h-[300px]">
+					<TabsContent value="tab-6" class="h-auto min-h-[300px]">
 						<Tabs value="tab-6" class=" items-center">
 							<TabsList
 								class="border-border h-auto gap-2 rounded-full border-b bg-zinc-800 px-10 py-2 text-zinc-400"
@@ -61,6 +63,53 @@
 							<TabsContent value="tab-6">
 								<Deactivate username={data.user?.username} />
 							</TabsContent>
+						</Tabs>
+					</TabsContent>
+					<TabsContent value="tab-5" class="h-auto min-h-[300px]">
+						<Tabs value="tab-4" class=" items-center">
+							<TabsList
+								class="border-border h-auto gap-2 rounded-full border-b bg-zinc-800 px-10 py-2 text-zinc-400"
+							>
+								<TabsTrigger
+									value="tab-4"
+									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+								>
+									Connect
+								</TabsTrigger>
+
+								<TabsTrigger
+									value="tab-5"
+									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+								>
+									Profile
+								</TabsTrigger>
+							</TabsList>
+
+							<TabsContent value="tab-4">
+								<div class=" *:not-first:mt-4 mt-8">
+									<Textarea user={data.user} />
+								</div>
+							</TabsContent>
+							<TabsContent value="tab-5">
+								<Profile />
+							</TabsContent>
+						</Tabs>
+					</TabsContent>
+
+					<TabsContent value="tab-4" class="h-auto min-h-[300px]">
+						<Tabs value="tab-4" class=" items-center">
+							<TabsList
+								class="border-border h-auto gap-2 rounded-full border-b bg-zinc-800 px-10 py-2 text-zinc-400"
+							>
+								<TabsTrigger
+									value="tab-4"
+									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+								>
+									Profile
+								</TabsTrigger>
+							</TabsList>
+
+							<TabsContent value="tab-4"></TabsContent>
 						</Tabs>
 					</TabsContent>
 				</div>
