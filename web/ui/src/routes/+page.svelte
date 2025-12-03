2ab9e4f40f32fae3bea7fc31c3386bd7ba5c2abb
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-12-03 20:16:11 +0800

    Refactor layout and styling in +page.svelte for improved responsiveness and consistency

--- web/ui/src/routes/+page.svelte
+++ web/ui/src/routes/+page.svelte
@@ -17,39 +17,44 @@
 	let currentTab = 'tab-0';
 </script>
 
-<div class="flex min-h-screen items-start justify-center overflow-hidden bg-black">
-	<div class="fixed mx-auto w-full max-w-7xl py-4">
+<div
+	class="flex min-h-screen items-start justify-center overflow-x-hidden bg-black md:overflow-hidden"
+>
+	<div class="mx-auto w-full max-w-7xl py-4 md:fixed">
 		<div class="px-4 py-4">
 			<Header />
 		</div>
 
 		<Tabs
 			value={currentTab}
-			class="flex max-h-[calc(85dvh-3rem)] w-full flex-col gap-8 md:max-h-[calc(100dvh-3rem)] md:flex-row"
+			class="flex w-full flex-col gap-8 md:max-h-[calc(100dvh-3rem)] md:flex-row"
 		>
-			<div class="grid w-full grid-cols-1 gap-4 overflow-y-auto px-10 pb-20 md:grid-cols-2">
+			<div
+				class="grid w-full grid-cols-1 gap-4 px-4 pb-20 md:grid-cols-2 md:overflow-y-auto md:px-10"
+			>
 				<div
-					class="border-border relative col-span-1 min-w-[300px] rounded-lg border bg-zinc-900 p-10 md:col-span-2 md:min-h-[480px] md:overflow-y-hidden"
+					class="border-border relative col-span-1 min-w-full rounded-lg border bg-zinc-900 p-6 md:col-span-2 md:min-h-[480px] md:min-w-[300px] md:overflow-y-hidden md:p-10"
 				>
-					<TabsContent value="tab-0" class="h-[600px] md:min-h-[400px] xl:h-[600px]">
-						<Tabs value="tab-1" class=" items-center">
+					<TabsContent value="tab-0" class="min-h-[480px] md:h-[600px] xl:h-[600px]">
+						<Tabs value="tab-1" class="items-center">
 							<TabsList
-								class="border-border h-auto  gap-2 rounded-full border-b bg-zinc-800 px-10 py-2 text-zinc-400"
+								class="border-border h-auto gap-2 overflow-x-auto rounded-full border-b bg-zinc-800 px-4 py-2 text-sm text-zinc-400 md:px-10 md:text-base"
 							>
 								<TabsTrigger
 									value="tab-1"
-									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+									class="relative cursor-pointer after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
 								>
 									Home
 								</TabsTrigger>
 
 								<TabsTrigger
 									value="tab-2"
-									class="relative cursor-pointer after:absolute  after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5  hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
+									class="relative cursor-pointer after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 hover:text-white hover:after:bg-white data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=active]:shadow-none data-[state=active]:after:bg-white data-[state=active]:hover:text-white"
 								>
 									Settings
 								</TabsTrigger>
 							</TabsList>
+
 							<TabsContent value="tab-1">
 								<Page {data} />
 								<div
@@ -77,11 +82,12 @@
 
 									{#if data.albums.length === 0}
 										<Skeleton
-											class="md:h-50 md:w-50 h-35 w-35 mx-auto rounded-2xl  bg-zinc-800 object-cover"
+											class="md:h-50 md:w-50 h-35 w-35 mx-auto rounded-2xl bg-zinc-800 object-cover"
 										/>
 									{/if}
 								</div>
 							</TabsContent>
+
 							<TabsContent value="tab-2">
 								<Settings />
 							</TabsContent>
