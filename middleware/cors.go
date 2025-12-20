31b8f0af8e700704699b943c1e91bfcda33a7704
ayunami2000 <spwilliamsiam@gmail.com>
2025-12-19 20:18:58 -0500

    Fix rewriting and CORS for clients that do not append .view

--- middleware/cors.go
+++ middleware/cors.go
@@ -4,13 +4,21 @@ import (
 	"hifi/config"
 	"net/http"
 	"slices"
+	"strings"
 )
 
 // -------------------- CORS --------------------
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if slices.Contains(config.ValidPaths, r.URL.Path) {
+		path := r.URL.Path
+
+		// Ensure path ends with .view (for Tempo)
+		if !strings.HasSuffix(path, ".view") {
+			path += ".view"
+		}
+
+		if slices.Contains(config.ValidPaths, path) {
 			w.Header().Set(config.HeaderAllowOrigin, config.CORSAllowOrigin)
 		}
 		next.ServeHTTP(w, r)
