d861ef7c0e97028d9f6f888697d59e1679260493
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2025-12-26 09:03:52 +0800

    Fix Tidal search limit comment for clarity

--- middleware/search3.go
+++ middleware/search3.go
@@ -37,7 +37,7 @@ func search3(search string, user string, w http.ResponseWriter) {
 	}
 	q := tidalURL.Query()
 	q.Set("query", qu)
-	q.Set("limit", "100") // Max limit = 1000
+	q.Set("limit", "100") // Max limit = 100
 	q.Set("offset", "0")
 	q.Set("types", "ARTISTS,ALBUMS,TRACKS")
 	q.Set("countryCode", "US")
